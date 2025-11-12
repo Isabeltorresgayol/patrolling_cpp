b5f1cc8d8dba9195174f2cd47c5a0f2de4f48e20
Isabel <isabel@al435576.uji.es>
2025-11-12 20:10:27 +0100

    V1.1: Change topic class

--- src/patrolling_cpp/src/patrolling_node.cpp
+++ src/patrolling_cpp/src/patrolling_node.cpp
@@ -1,22 +1,25 @@
 #include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("publisher_node");
-    auto publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("/goal_pose", 10);
 
-    geometry_msgs::msg::PoseStamped msg;
+    // Cambiamos el tipo de mensaje
+    auto publisher = node->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/goal_pose", 10);
+
+    geometry_msgs::msg::PoseWithCovarianceStamped msg;
     msg.header.frame_id = "map";
-    msg.pose.position.x = 2.0;
-    msg.pose.position.y = 3.0;
-    msg.pose.position.z = 0.0;
-    msg.pose.orientation.w = 1.0;
+    msg.pose.pose.position.x = 2.0;
+    msg.pose.pose.position.y = 3.0;
+    msg.pose.pose.position.z = 0.0;
+    msg.pose.pose.orientation.w = 1.0;
 
     publisher->publish(msg);
-    RCLCPP_INFO(node->get_logger(), "Meta publicada en /goal_pose");
+    RCLCPP_INFO(node->get_logger(), "Meta publicada en /goal_pose con PoseWithCovarianceStamped");
 
     rclcpp::shutdown();
     return 0;
 }
+
